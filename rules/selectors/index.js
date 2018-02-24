1999609632c8ed67f4796e3e3511216dbd773347
mitchreece <mitchellreece18@gmail.com>
2018-02-24 16:41:06 +0000

    set selector rules

---
+++ rules/selectors/index.js
@@ -0,0 +1,19 @@
+module.exports = {
+    'selector-max-id': [0, {
+        message: 'Unexpected use of id selector',
+    }],
+    'no-duplicate-selectors': true,
+    'selector-combinator-space-before': 'always',
+    'selector-combinator-space-after': 'always',
+    'selector-list-comma-space-before': 'never',
+    'selector-list-comma-space-after': 'always',
+    'selector-descendant-combinator-no-non-space': true,
+    'selector-pseudo-class-parentheses-space-inside': 'never',
+    'selector-attribute-quotes': 'never',
+    'selector-type-case': 'lower',
+    'selector-pseudo-element-case': 'lower',
+    'selector-pseudo-class-case': 'lower',
+    'rule-empty-line-before': ['always', {
+        'ignore': ['inside-block', 'after-comment'],
+    }],
+}
