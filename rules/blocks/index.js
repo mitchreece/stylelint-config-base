694749c53e383fe41cc096d5a075ec1e9a7ed523
mitchreece <mitchellreece18@gmail.com>
2018-02-24 16:40:32 +0000

    set block rules

---
+++ rules/blocks/index.js
@@ -0,0 +1,7 @@
+module.exports = {
+    'block-opening-brace-space-before': 'always',
+    'block-opening-brace-newline-after': 'always',
+    'block-closing-brace-newline-before': 'always',
+    'block-no-empty': true,
+    'at-rule-name-case': 'lower',
+}
